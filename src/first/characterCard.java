0ded9b5ea8a4573712a47b8d864ea8ec28c91941
Darfresor <31156832+Darfresor@users.noreply.github.com>
2017-12-09 22:43:13 +0300

    первая основа игры

---
+++ src/first/characterCard.java
@@ -0,0 +1,22 @@
+package first;
+
+public class characterCard {
+
+	void setParam() {
+		String gender; // Пол. 0 - нет пола, 1 - мужчина, 2 - женщина;
+		String level; // уровень. Может принимать значение от 1 - до 10. По умолчанию у всех 1  уровень.
+		String race;  // раса. 0 - человек, 1 - гном, 2 - эльф, 3 - хафлиинг
+		String profession; // Профессия. 0 - нет професии; 1- Маг, 2 - Воин, 3 - Клирик, 4 - Вор.
+		String head; //голова
+		String body; //тело
+		String leftHand; // левая рука
+		String rightHand; // правая рука
+		String leftLeg; // левая нога
+		String rightLeg; // правая нога
+		String TheBlessing; //благословления
+		String Curses; //проклятия
+		String inventory; //инвентарь
+		String  hiddenCArd; // скрыте карты на руках
+	}
+	
+}
---
+++ src/first/game.java
@@ -0,0 +1,22 @@
+package first;
+
+/**
+ * @author sking91
+ *
+ */
+public class game {
+		
+	
+			
+	public static void main(String[] args) {
+		
+		String version ="0.0.1";
+		
+		
+        System.out.println("ПК версия  настольной игры манчкин запущена");
+        System.out.println("Текущая версия "+version);
+        System.out.println("Меню");
+        System.out.println("1.Старт игры");
+        System.out.println("2.Выход из программы");
+    }
+}
